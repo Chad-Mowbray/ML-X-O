e3a6d03339fa577a6221dc06a2d0804439e8deb2
Chad-Mowbray <mowbray.chad@gmail.com>
2023-02-27 10:48:23 -0500

    adding training tests

--- backend/apistuff/training.py
+++ backend/apistuff/training.py
@@ -15,16 +15,25 @@ def get_train_test_split(samples):
 def save_model(model):
     pickle.dump(model, open("current_model.pickle", "wb"))
 
-def train(samples):
-    train_samples, test_samples = get_train_test_split(samples)
+def get_samples(train_samples, test_samples):
     X = [s["processed"] for s in train_samples]
     Y = [s["category"] for s in train_samples]
-    clf = GaussianNB()
-    clf.fit(X, Y)
     test_x = [s["processed"] for s in test_samples]
     test_y = [s["category"] for s in test_samples]
-    score = clf.score(test_x, test_y)
-    save_model(clf)
+    return X, Y, test_x, test_y
+
+def fit_model(X, Y):
+    clf = GaussianNB()
+    clf.fit(X, Y)
+    return clf
+
+
+def train(samples):
+    train_samples, test_samples = get_train_test_split(samples)
+    X, Y, test_x, test_y = get_samples(train_samples, test_samples)
+    model = fit_model(X, Y)
+    score = model.score(test_x, test_y)
+    save_model(model)
     return score
 
 
--- backend/egproj/settings.py
+++ backend/egproj/settings.py
@@ -76,14 +76,20 @@ WSGI_APPLICATION = 'egproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',  
     }
 }
 
